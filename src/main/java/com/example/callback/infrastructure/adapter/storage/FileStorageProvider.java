07cec8941b6c1952f43cf0e63262aa1028aa7acd
k9want <zndwlrnjr26@daum.net>
2025-11-26 11:45:45 +0900

    fix: 원문 그대로 로그 저장하도록 변경

--- src/main/java/com/example/callback/infrastructure/adapter/storage/FileStorageProvider.java
+++ src/main/java/com/example/callback/infrastructure/adapter/storage/FileStorageProvider.java
@@ -3,8 +3,10 @@ package com.example.callback.infrastructure.adapter.storage;
 import com.example.callback.callback.event.CallbackEvent;
 import com.example.callback.callback.service.port.StorageProvider;
 import com.example.callback.infrastructure.adapter.queue.CallbackPayload;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.FileWriter;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Primary
 @Component
+@RequiredArgsConstructor
 public class FileStorageProvider implements StorageProvider {
+
+  private final ObjectMapper objectMapper;
 
   @Override
   public void save(CallbackEvent data) {
     CallbackPayload request = data.getPayload();
 
+
+
     try (FileWriter writer = new FileWriter("callback_log.txt", true)) {
-      writer.write(request.getMessage() + "\n");
+      writer.write(objectMapper.writeValueAsString(data.getPayload()) + System.lineSeparator());
       log.info("Success: Data saved to file.");
     } catch (IOException e) {
       log.error("Error: File write failed.", e);
